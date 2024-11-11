677823b77e5a9ffdab24e78ca2883a9dbfbc47f2
Project Owner <projectowner@company.com>
2024-11-11 20:12:20 +0100

    Fixed issue with folder permissions for the e4l-mysql and e4l-news

--- Scripts/start_product.sh
+++ Scripts/start_product.sh
@@ -4,10 +4,24 @@
 BACKEND_PATH="$HOME/1.e4l_DevOps_Pipeline/s1-create-skeleton/lu.uni.e4l.platform.api.dev"
 FRONTEND_PATH="$HOME/1.e4l_DevOps_Pipeline/s1-create-skeleton/lu.uni.e4l.platform.frontend.dev"
 
+# Create the e4l-mysql directory if it doesn't exist and set permissions
+if [ ! -d "${HOME}/e4l-mysql" ]; then
+    echo "Creating ${HOME}/e4l-mysql directory..."
+    sudo mkdir -p "${HOME}/e4l-mysql"
+    sudo chmod -R 777 "${HOME}/e4l-mysql"
+fi
+
+# Create the e4l-news directory if it doesn't exist and set permissions
+if [ ! -d "${HOME}/e4l-news" ]; then
+    echo "Creating ${HOME}/e4l-news directory..."
+    sudo mkdir -p "${HOME}/e4l-news"
+    sudo chmod -R 777 "${HOME}/e4l-news"
+fi
+
 # Step 1: Start the Database Container
 echo "Starting the database container..."
 cd "$BACKEND_PATH" || exit
-sudo HOME="$HOME" docker-compose -f ./docker/docker-compose.db.yml up -d
+sudo docker-compose -f ./docker/docker-compose.db.yml up -d
 
 # Step 2: Start the Backend Container
 echo "Starting the backend container..."
@@ -17,6 +31,6 @@ sudo docker-compose -f ./docker/docker-compose.backend.pre-prod.yml up -d
 # Step 3: Start the Frontend Container
 echo "Starting the frontend container..."
 cd "$FRONTEND_PATH" || exit
-sudo HOME="$HOME" docker-compose -f ./e4l.frontend.docker/docker-compose.frontend.pre-prod.yml up -d
+sudo docker-compose -f ./e4l.frontend.docker/docker-compose.frontend.pre-prod.yml up -d
 
 echo "All containers have been started."
