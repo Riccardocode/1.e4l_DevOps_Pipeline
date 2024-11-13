4627bae8a47b2661249fac562ab1e0f3ad15226f
ProjectOwner <scaliariccardo_89@hotmail.it>
2024-11-13 19:19:11 +0100

    S2-Automate-build

---
+++ s2-automate-build/integration-server/playbook/provision_scripts/setup-gitlab-runner.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install GitLab Runner
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
+sudo apt-get install -y gitlab-runner
+
+# Retrieve the runner registration token (only if this is run on the GitLab server)
+# If not on the GitLab server, copy the token from GitLab UI: Settings > CI / CD > Runners
+token=$(sudo gitlab-rails runner -e production "puts Gitlab::CurrentSettings.current_application_settings.runners_registration_token")
+
+# Check if token retrieval was successful
+if [ -z "$token" ]; then
+  echo "Error: Failed to retrieve the runner registration token. Please ensure the token is set correctly."
+  exit 1
+fi
+
+# Register the GitLab Runner
+sudo gitlab-runner register \
+  --non-interactive \
+  --url "http://192.168.56.9/gitlab/" \
+  --registration-token "$token" \
+  --description "docker-runner" \
+  --tag-list "docker" \
+  --executor "docker" \
+  --docker-image "alpine:latest" \
+  --run-untagged="true"
+
+# Update the privileged setting in config.toml
+CONFIG_FILE="/etc/gitlab-runner/config.toml"
+sudo sed -i 's/privileged = false/privileged = true/' "$CONFIG_FILE"
+
